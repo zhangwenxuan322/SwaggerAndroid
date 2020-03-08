79086ddf9f0ef59cc33859e02e06388b0694c336
zhangwenxuan322 <798043150@qq.com>
2020-03-08 13:58:12 +0800

    自定义聊天界面导航栏

--- app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
+++ app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
@@ -1,8 +1,14 @@
 package com.friend.swagger.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.friend.swagger.R;
 
@@ -12,5 +18,34 @@ public class ConversationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // 返回
+            case android.R.id.home:
+                new Thread () {
+                    public void run () {
+                        try {
+                            Instrumentation inst= new Instrumentation();
+                            inst.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+                return true;
+            // 用户详情
+            case R.id.user_profile:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.conversation_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
