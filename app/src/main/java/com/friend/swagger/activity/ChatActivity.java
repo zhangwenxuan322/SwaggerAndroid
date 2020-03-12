4fd713236fd4ad88c0536f7ec05aa8a0a8663434
zhangwenxuan322 <798043150@qq.com>
2020-03-12 15:56:03 +0800

    原生DrawerLayout实现侧边栏

--- app/src/main/java/com/friend/swagger/activity/ChatActivity.java
+++ app/src/main/java/com/friend/swagger/activity/ChatActivity.java
@@ -1,7 +1,11 @@
 package com.friend.swagger.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import io.rong.imkit.RongIM;
 import io.rong.imkit.fragment.ConversationListFragment;
@@ -12,22 +16,30 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.friend.swagger.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final String EXTRA_ACCOUNT =
             "indi.friend.swagger.EXTRA_ACCOUNT";
     public static final String EXTRA_TOKEN =
             "indi.friend.swagger.EXTRA_TOKEN";
     private FloatingActionButton startChatBtn;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         startChatBtn = findViewById(R.id.startChat);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
         initView();
         RongIM.getInstance().setConversationClickListener(new MyConversationClickListener());
     }
@@ -51,7 +63,11 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setBackgroundColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     private void initFragment() {
@@ -76,8 +92,14 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.chat_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.contacts:
+                Toast.makeText(this, "contacts", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
+++ app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
@@ -20,32 +20,4 @@ public class ConversationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_conversation);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // 返回
-            case android.R.id.home:
-                new Thread () {
-                    public void run () {
-                        try {
-                            Instrumentation inst= new Instrumentation();
-                            inst.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
-                        } catch(Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }.start();
-                return true;
-            // 用户详情
-            case R.id.user_profile:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.conversation_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
 }
