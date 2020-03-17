82a5efdc5f08b7c71d456e6def0ce0a627409d92
zhangwenxuan322 <798043150@qq.com>
2020-03-17 12:14:35 +0800

    新增用户详情页

---
+++ app/src/main/java/com/friend/swagger/activity/UserDeatailActivity.java
@@ -0,0 +1,16 @@
+package com.friend.swagger.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.friend.swagger.R;
+
+public class UserDeatailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_deatail);
+    }
+}
