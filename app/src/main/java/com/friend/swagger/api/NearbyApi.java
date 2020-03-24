b58aceeb034b899849378c70366554c3b094249b
zhangwenxuan322 <798043150@qq.com>
2020-03-24 09:25:58 +0800

    新增附近的人接口

---
+++ app/src/main/java/com/friend/swagger/api/NearbyApi.java
@@ -0,0 +1,25 @@
+package com.friend.swagger.api;
+
+import com.friend.swagger.entity.NearbyUser;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * @Author ZhangWenXuan
+ * @Date 2020-03-23 17:20
+ **/
+public interface NearbyApi {
+    /**
+     * 获取附近的人
+     *
+     * @return
+     */
+    @GET("nearby/list")
+    Call<List<NearbyUser>> getNearbyUsers(@Query("lon") Double lon,
+                                          @Query("lat") Double lat,
+                                          @Query("limit") Double limit);
+}
