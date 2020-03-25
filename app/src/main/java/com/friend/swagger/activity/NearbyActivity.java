b7932baa98280c33c33fa1352a1d33d46f310cdf
zhangwenxuan322 <798043150@qq.com>
2020-03-25 14:22:48 +0800

    附近的人页添加范围选择器

--- app/src/main/java/com/friend/swagger/activity/NearbyActivity.java
+++ app/src/main/java/com/friend/swagger/activity/NearbyActivity.java
@@ -10,8 +10,12 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.friend.swagger.R;
@@ -20,6 +24,7 @@ import com.friend.swagger.api.NearbyApi;
 import com.friend.swagger.api.RetrofitService;
 import com.friend.swagger.entity.NearbyUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NearbyActivity extends AppCompatActivity {
@@ -32,13 +37,23 @@ public class NearbyActivity extends AppCompatActivity {
     // api
     private NearbyApi nearbyApi;
     // 范围限制
-    private Double limit;
+    private Double limit = 10.0;
     // intent
     private Intent intent;
     // recycler
     private RecyclerView nearbyRecyclerView;
     private NearbyAdapter nearbyAdapter;
     private RecyclerView.LayoutManager nearbyLayoutManager;
+    // Seekbar
+    private SeekBar seekBar;
+    private TextView seekbarValueText;
+    // 经纬度
+    Double lon;
+    Double lat;
+    // 用户id
+    String id;
+    // 附近的人列表
+    List<NearbyUser> nearbyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,47 +61,79 @@ public class NearbyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nearby);
         nearbyApi = RetrofitService.createService(NearbyApi.class);
         intent = getIntent();
-        limit = 10000.0;
+        seekBar = findViewById(R.id.seekbar);
+        seekbarValueText = findViewById(R.id.seekbar_value);
+        seekBar.getThumb().setColorFilter(Color.parseColor("#2c3e50"), PorterDuff.Mode.SRC_ATOP);
+        seekBar.getProgressDrawable().setColorFilter(Color.parseColor("#2c3e50"), PorterDuff.Mode.SRC_ATOP);
         initToolbar();
         initRecyclerView();
+        setSeekbarListener();
+    }
+
+    private void setSeekbarListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekbarValueText.setText(Integer.toString(progress));
+                limit = new Double(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                requestNearbyUsers(lon, lat, id);
+            }
+        });
     }
 
     private void initRecyclerView() {
-        Double lon = intent.getDoubleExtra(EXTRA_LON, 0.0);
-        Double lat = intent.getDoubleExtra(EXTRA_LAT, 0.0);
-        String id = String.valueOf(intent.getIntExtra(EXTRA_ID, 0));
+        lon = intent.getDoubleExtra(EXTRA_LON, 0.0);
+        lat = intent.getDoubleExtra(EXTRA_LAT, 0.0);
+        id = String.valueOf(intent.getIntExtra(EXTRA_ID, 0));
+        nearbyList = new ArrayList<>();
+        nearbyRecyclerView = findViewById(R.id.nearby_recycler);
+        nearbyRecyclerView.setHasFixedSize(true);
+        nearbyLayoutManager = new LinearLayoutManager(NearbyActivity.this);
+        nearbyRecyclerView.setLayoutManager(nearbyLayoutManager);
+        nearbyAdapter = new NearbyAdapter(nearbyList);
+        nearbyRecyclerView.setAdapter(nearbyAdapter);
+        nearbyRecyclerView.addItemDecoration(new DividerItemDecoration(NearbyActivity.this,
+                DividerItemDecoration.VERTICAL));
+        nearbyAdapter.setNearbyItemClickListener(new NearbyAdapter.OnNearbyItemClickListener() {
+            @Override
+            public void onNearbyItemClick(int position) {
+                Toast.makeText(NearbyActivity.this, nearbyList.get(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestNearbyUsers(lon, lat, id);
+    }
+
+    private void requestNearbyUsers(Double lon, Double lat, String id) {
         nearbyApi.getNearbyUsers(lon, lat, limit).enqueue(new Callback<List<NearbyUser>>() {
             @Override
             public void onResponse(Call<List<NearbyUser>> call, Response<List<NearbyUser>> response) {
-                List<NearbyUser> nearbyList = response.body();
-                if (nearbyList == null) {
+                nearbyList.clear();
+                List<NearbyUser> list = response.body();
+                if (list == null) {
                     Toast.makeText(NearbyActivity.this, "请求失败", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 // 从列表中删除正在使用的用户
                 int index = 0;
-                for (NearbyUser nearbyUser : nearbyList) {
+                for (NearbyUser nearbyUser : list) {
                     if (nearbyUser.getNearbyId().equals(id)) {
-                        index = nearbyList.indexOf(nearbyUser);
+                        index = list.indexOf(nearbyUser);
                         break;
                     }
                 }
-                nearbyList.remove(nearbyList.get(index));
-                nearbyRecyclerView = findViewById(R.id.nearby_recycler);
-                nearbyRecyclerView.setHasFixedSize(true);
-                nearbyLayoutManager = new LinearLayoutManager(NearbyActivity.this);
-                nearbyRecyclerView.setLayoutManager(nearbyLayoutManager);
-                nearbyAdapter = new NearbyAdapter(nearbyList);
-                nearbyRecyclerView.setAdapter(nearbyAdapter);
-                nearbyRecyclerView.addItemDecoration(new DividerItemDecoration(NearbyActivity.this,
-                        DividerItemDecoration.VERTICAL));
-                nearbyAdapter.setNearbyItemClickListener(new NearbyAdapter.OnNearbyItemClickListener() {
-                    @Override
-                    public void onNearbyItemClick(int position) {
-                        Toast.makeText(NearbyActivity.this, nearbyList.get(position).toString(),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
+                list.remove(list.get(index));
+                nearbyList.addAll(list);
+                nearbyAdapter.notifyDataSetChanged();
             }
 
             @Override
