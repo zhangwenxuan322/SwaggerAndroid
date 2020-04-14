6f556ce9ef7de672707a48328825417274b45949
zhangwenxuan322 <798043150@qq.com>
2020-04-14 23:07:22 +0800

    delete

--- app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
+++ app/src/main/java/com/friend/swagger/activity/ConversationActivity.java
@@ -3,11 +3,8 @@ package com.friend.swagger.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.friend.swagger.R;
 
