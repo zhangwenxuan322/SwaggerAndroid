e21d2d7859f8640713bcab03ee9003364c5c5645
zhangwenxuan322 <798043150@qq.com>
2020-05-05 10:36:36 +0800

    完成软件更新功能

--- app/src/main/java/com/friend/swagger/activity/SettingsActivity.java
+++ app/src/main/java/com/friend/swagger/activity/SettingsActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,22 +15,29 @@ import android.widget.Toast;
 
 import com.friend.swagger.R;
 import com.friend.swagger.adapter.SettingsAdapter;
+import com.friend.swagger.api.RetrofitService;
+import com.friend.swagger.api.UpdateApi;
+import com.friend.swagger.common.Constant;
 import com.open.hule.library.entity.AppUpdate;
 import com.open.hule.library.utils.UpdateManager;
+import com.tamsiree.rxtool.RxAppTool;
 import com.tamsiree.rxtool.view.RxToast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
     private RecyclerView settingsRecyclerView;
     private SettingsAdapter settingsAdapter;
     private RecyclerView.LayoutManager settingsLayoutManager;
+    private UpdateApi updateApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        updateApi = RetrofitService.createService(UpdateApi.class);
         initToolbar();
         initRecyclerView();
     }
@@ -46,18 +56,39 @@ public class SettingsActivity extends AppCompatActivity {
         settingsAdapter.setSettingsItemClickListener(new SettingsAdapter.OnSettingsItemClickListener() {
             @Override
             public void onSettingsItemClick(int position) {
+                int curVersion = RxAppTool.getAppVersionCode(SettingsActivity.this);
                 if (optionList.get(position).equals("检查更新")) {
-                    AppUpdate appUpdate = new AppUpdate.Builder()
-                            //更新地址（必须）
-                            .newVersionUrl("https://imtt.dd.qq.com/16891/8EC4E86B648D57FDF114AF5D3002C09B.apk")
-                            // 版本号（非必须）
-                            .newVersionCode("v1.7")
-                            // 文件大小（非必须）
-                            .fileSize("5.8M")
-                            // 更新内容（非必填，默认“1.用户体验优化\n2.部分问题修复”）
-                            .updateInfo("1.用户体验优化\n2.部分问题修复")
-                            .build();
-                    new UpdateManager().startUpdate(SettingsActivity.this, appUpdate);
+                    updateApi.getLatestApkInfo().enqueue(new Callback<Map<String, Object>>() {
+                        @Override
+                        public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
+                            Map<String, Object> map = response.body();
+                            if (map == null) {
+                                RxToast.error("请求错误");
+                                return;
+                            }
+                            int apkVersion = Integer.valueOf(map.get("apkCode").toString());
+                            String apkFileName = map.get("apkName").toString();
+                            String apkDesc = map.get("apkDesc").toString();
+                            if (apkVersion > curVersion) {
+                                AppUpdate appUpdate = new AppUpdate.Builder()
+                                        //更新地址（必须）
+                                        .newVersionUrl(Constant.remoteUrl + "apk/" + apkFileName)
+                                        // 版本号（非必须）
+                                        .newVersionCode("v" + apkVersion)
+                                        // 更新内容（非必填，默认“1.用户体验优化\n2.部分问题修复”）
+                                        .updateInfo(apkDesc)
+                                        .build();
+                                new UpdateManager().startUpdate(SettingsActivity.this, appUpdate);
+                            } else {
+                                RxToast.info("已是最新版");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Map<String, Object>> call, Throwable t) {
+                            RxToast.error("请求错误");
+                        }
+                    });
                 }
             }
         });
